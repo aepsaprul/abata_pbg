727ce4efdcc69ea28b38ff5a7e15927af26c3e2f
aep <aepsaprul18@gmail.com>
2021-02-27 15:01:59 +0700

    update halaman welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -56,6 +56,32 @@
                 border-collapse: collapse;
                 border-spacing: 0;
             }
+            /**
+            * For modern browsers
+            * 1. The space content is one way to avoid an Opera bug when the
+            *    contenteditable attribute is included anywhere else in the document.
+            *    Otherwise it causes space to appear at the top and bottom of elements
+            *    that are clearfixed.
+            * 2. The use of `table` rather than `block` is only necessary if using
+            *    `:before` to contain the top-margins of child elements.
+            */
+            .cf:before,
+            .cf:after {
+                content: " "; /* 1 */
+                display: table; /* 2 */
+            }
+
+            .cf:after {
+                clear: both;
+            }
+
+            /**
+            * For IE 6/7 only
+            * Include this rule to trigger hasLayout and contain floats.
+            */
+            .cf {
+                *zoom: 1;
+            }
         </style>
 
         <style>
@@ -69,10 +95,86 @@
                 background-color: #176BB3;
             }
             .header .container {
-                width: 1200px;
+                width: 1300px;
                 height: 60px;
-                background-color: red;
                 margin: auto;
+            }
+            .header .container .logo,
+            .header .container .nav,
+            .header .container .medsos {
+                float: left;
+            }
+            /*  header logo  */
+            .header .container .logo {
+                width: 250px;
+                box-sizing: border-box;
+            }
+            .header .container .logo img {
+                line-height: 60px;
+                max-width: 100px;
+                padding: 5px;
+            }
+
+            /* header nav  */
+            .header .container .nav {
+                width: 800px;
+                box-sizing: border-box;
+                text-align: center;
+            }
+            .header .container .nav a {
+                color: #FFDD00;
+                text-decoration: none;
+                line-height: 60px;
+                text-transform: uppercase;
+                font-weight: bold;
+            }
+
+            /* header medsos  */
+            .header .container .medsos {
+                width: 250px;
+                line-height: 60px;
+                padding: 13px;
+                box-sizing: border-box;
+                text-align: right;
+            }
+            .header .container .medsos img {
+                max-width: 35px;
+                padding-left: 10px;
+            }
+            .content {
+                width: 100%;
+            }
+            .content .container {
+                width: 1300px;
+                margin: auto;
+            }
+            .content .container .maskot {
+                float: left;
+                width: 500px;
+                text-align: right;
+            }
+            .content .container .welcome {
+                float: right;
+                width: 800px;
+                text-align: center;
+            }
+            .content .container .welcome p {
+                padding: 200px 0;
+            }
+            .content .container .welcome p .satu {
+                background-color: #176BB3;
+                padding: 10px 20px;
+                text-transform: uppercase;
+                font-weight: bold;
+                font-family: Arial, Helvetica, sans-serif;
+                color: #FFF;
+                font-size: 1cm;
+            }
+            .content .container .welcome p .dua {
+                font-size: 1cm;
+                text-transform: uppercase;
+                color: #176BB3;
+                font-family: Arial, Helvetica, sans-serif;
             }
         </style>
     </head>
@@ -80,18 +182,31 @@
         <div class="header">
             <div class="container">
                 <div class="logo">
-
+                    <img src="{{ asset('assets/img/logo.png') }}" alt="logo abata">
                 </div>
                 <div class="nav">
-
+                    <a href="#">Home</a>
                 </div>
                 <div class="medsos">
-
+                    <img src="{{ asset('assets/img/youtube.png') }}" alt="youtube">
+                    <img src="{{ asset('assets/img/instagram.png') }}" alt="instagram">
+                    <img src="{{ asset('assets/img/facebook.png') }}" alt="facebook">
                 </div>
             </div>
         </div>
-        <div class="content">
-
+        <div class="content cf">
+            <div class="container">
+                <div class="maskot">
+                    <img src="{{ asset('assets/img/maskot.png') }}" alt="maskot">
+                </div>
+                <div class="welcome">
+                    <p>
+                        <span class="satu"> selamat datang </span>
+                        <br><br><br>
+                        <span class="dua">abata cabang purbalingga</span>
+                    </p>
+                </div>
+            </div>
         </div>
     </body>
 </html>
